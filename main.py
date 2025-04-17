89c71d0020cb7303c7450de1dad963dc9c2194da
rianbutala <rianbutala@gmail.com>
2025-04-16 17:20:18 -0700

    reenable test spark

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import Union
 from fastapi import FastAPI, WebSocket
 from gpiozero import Motor
-# import sparkmotor
+import sparkmotor
 
 app = FastAPI()
-# testmotor = sparkmotor.PWMController(pin=12)  # Update with the correct GPIO pin
+testmotor = sparkmotor.PWMController(pin=12)  # Update with the correct GPIO pin
 
 @app.get("/")
 def read_root():
@@ -17,4 +17,4 @@ async def websocket_endpoint(websocket: WebSocket):
         data = await websocket.receive_text()
         await websocket.send_text(f"Message text was: {data}")
         print("Received", data)
-        # testmotor.set_power(float(data))
+        testmotor.set_power(float(data))
